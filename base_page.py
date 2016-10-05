798f9517a91c3dcf2fe868ddbad6caff3bc715d2
klindtwj <klindtwj@onid.oregonstate.edu>
2016-10-05 10:40:19 -0700

    Update base_page.py

--- base_page.py
+++ base_page.py
@@ -3,4 +3,4 @@ import webapp2
 class HelloWorld(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-type'] = 'test/plain'
-    self.response.write('Hello world')
+    self.response.write('Hello again world')
